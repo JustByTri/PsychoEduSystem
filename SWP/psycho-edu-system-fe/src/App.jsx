620789703048f7ffc313d78ecbb683792eed11f0
Dev-FStacker <bennguyen.contact@gmail.com>
2025-01-18 16:40:32 +0700

    Add component: not found page

--- SWP/psycho-edu-system-fe/src/App.jsx
+++ SWP/psycho-edu-system-fe/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/main/HomePage";
 import MainLayout from "./components/Layouts/MainLayout";
+import NotFoundPage from "./pages/error/NotFoundPage";
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
           {/* Nested Routes */}
           <Route index element={<HomePage />} />
         </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
