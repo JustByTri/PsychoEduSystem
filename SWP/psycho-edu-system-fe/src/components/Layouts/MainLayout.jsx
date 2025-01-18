260b98b7145294e72b9565730a764627ab6ae539
Dev-FStacker <bennguyen.contact@gmail.com>
2025-01-18 13:14:59 +0700

    Add component: notification

--- SWP/psycho-edu-system-fe/src/components/Layouts/MainLayout.jsx
+++ SWP/psycho-edu-system-fe/src/components/Layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navigation/NavBar";
+import Notification from "../Notifications/Notification";
 
 const MainLayout = () => {
   return (
     <>
+      <Notification />
       <Navbar />
-      <Outlet /> {/* Dynamic routing for child components */}
+      <Outlet />
     </>
   );
 };
