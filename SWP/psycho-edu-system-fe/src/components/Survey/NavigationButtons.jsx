9cce4826e32fbb2402aa6ae35287aeb76d5e8f0c
Dev-FStacker <bennguyen.contact@gmail.com>
2025-01-31 01:27:42 +0700

    Add feature: survey result page

---
+++ SWP/psycho-edu-system-fe/src/components/Survey/NavigationButtons.jsx
@@ -0,0 +1,28 @@
+const NavigationButtons = ({
+  currentQuestionIndex,
+  totalQuestions,
+  isAnswered,
+  handlePreviousQuestion,
+  handleNextQuestion,
+}) => {
+  return (
+    <div className="flex justify-between items-center mt-6">
+      <button
+        onClick={handlePreviousQuestion}
+        disabled={currentQuestionIndex === 0}
+        className="px-6 py-2 bg-gray-400 text-white rounded-lg hover:bg-gray-500 disabled:opacity-50 transition-all duration-300"
+      >
+        Previous
+      </button>
+      <button
+        onClick={handleNextQuestion}
+        disabled={!isAnswered}
+        className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:opacity-50 transition-all duration-300"
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default NavigationButtons;
