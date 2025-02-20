a4580f23cac35ce73228802f7feda328fe4760a8
Dev-FStacker <bennguyen.contact@gmail.com>
2025-02-20 22:24:08 +0700

    Fix css for portal

--- SWP/psycho-edu-system-fe/src/components/Layouts/PortalLayout.jsx
+++ SWP/psycho-edu-system-fe/src/components/Layouts/PortalLayout.jsx
@@ -1,15 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import SideBar from "../Navigation/SideBar";
+
 const PortalLayout = () => {
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col h-screen">
+      {/* Header */}
       <Header />
+
+      {/* Main layout with Sidebar and Content */}
       <div className="flex flex-1 overflow-hidden">
-        <div className="flex-shrink-0">
+        {/* Sidebar (Fixed height, no scrolling) */}
+        <div className="flex-shrink-0 h-full">
           <SideBar />
         </div>
-        <div className="flex-1 overflow-auto">
+
+        {/* Outlet (Scrollable Content) */}
+        <div className="flex-1 overflow-auto p-4">
           <Outlet />
         </div>
       </div>
