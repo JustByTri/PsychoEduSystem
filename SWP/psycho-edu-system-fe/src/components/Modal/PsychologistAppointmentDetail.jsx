a1fac40aa3c8bfc61f3d4ba32f31870b2089b380
NET15_Quốc Thịnh <thinhpdqse171589@fpt.edu.vn>
2025-03-18 11:57:57 +0700

    Fix: import service

--- SWP/psycho-edu-system-fe/src/components/Modal/PsychologistAppointmentDetail.jsx
+++ SWP/psycho-edu-system-fe/src/components/Modal/PsychologistAppointmentDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { format, startOfDay, parse } from "date-fns";
 import { motion } from "framer-motion";
 import FeedbackForm from "./FeedbackForm";
+import apiService from "../../services/apiService";
 
 const PsychologistAppointmentDetail = ({
   isOpen,
