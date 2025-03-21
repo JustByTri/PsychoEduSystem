0bf31fa7284ae8c4585a168e400fc5802e79ee44
Mai Trinh Nguyen <trinhntmse170454@fpt.edu.vn>
2025-03-21 12:33:55 +0700

    check Blog

---
+++ SWP/psycho-edu-system-be/DAL/Entities/UserTargetProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class UserTargetProgram
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+        public Guid ProgramId { get; set; }
+        public DateTime JoinDate { get; set; }
+        public virtual User User { get; set; }
+        public virtual TargetProgram TargetProgram { get; set; }
+    }
+}
+
