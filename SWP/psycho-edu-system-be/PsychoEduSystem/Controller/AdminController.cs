35535ef2b4261cc006db9c567504ec293af1ff4c
Dev-FStacker <bennguyen.contact@gmail.com>
2025-04-01 01:01:19 +0700

    feat: admin controller

---
+++ SWP/psycho-edu-system-be/PsychoEduSystem/Controller/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using BLL.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PsychoEduSystem.Controller
+{
+    [Route("api/admin")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public AdminController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet("total-users")]
+        public async Task<IActionResult> GetTotalUserAsync()
+        {
+            var response = await _userService.GetTotalUserAsync();
+            return Ok(response);
+        }
+        [HttpGet("total-parents")]
+        public async Task<IActionResult> GetTotalParentAsync()
+        {
+            var response = await _userService.GetTotalParentAsync();
+            return Ok(response);
+        }
+        [HttpGet("total-classes")]
+        public async Task<IActionResult> GetTotalClassAsync()
+        {
+            var response = await _userService.GetTotalClassAsync();
+            return Ok(response);
+        }
+        [HttpGet("total-target-programs")]
+        public async Task<IActionResult> GetTotalTargetProramAsync()
+        {
+            var response = await _userService.GetTotalTargetProramAsync();
+            return Ok(response);
+        }
+        [HttpGet("total-appointments")]
+        public async Task<IActionResult> GetTotalAppointmentAsync()
+        {
+            var response = await _userService.GetTotalAppointmentAsync();
+            return Ok(response);
+        }
+    }
+}
